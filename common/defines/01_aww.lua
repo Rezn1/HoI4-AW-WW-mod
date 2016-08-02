9066d670c017f44c92af326bb1c5f91190d8842a
Rezn1 <jaro369@gmail.com>
2016-08-03 01:32:57 +0200

    0.1

---
+++ common/defines/01_aww.lua
@@ -0,0 +1,3 @@
+NDefines.NDiplomacy.GUARANTEE_COST = 2000 -- no guarantees
+NDefines.NDiplomacy.TENSION_GUARANTEE = 100 -- redundant
+NDefines.NCountry.LOCAL_MANPOWER_ACCESSIBLE_NON_CORE_FACTOR = 0.05 -- more manpower from conquered land
