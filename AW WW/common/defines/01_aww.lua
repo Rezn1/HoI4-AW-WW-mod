09c382972c42c579ef2ac66fba7a44727d3224d0
Rezn1 <jaro369@gmail.com>
2016-08-03 18:02:00 +0200

    no military access

--- AW WW/common/defines/01_aww.lua
+++ AW WW/common/defines/01_aww.lua
@@ -1,3 +1,4 @@
 NDefines.NDiplomacy.GUARANTEE_COST = 2000 -- no guarantees
 NDefines.NDiplomacy.TENSION_GUARANTEE = 100 -- redundant
 NDefines.NCountry.LOCAL_MANPOWER_ACCESSIBLE_NON_CORE_FACTOR = 0.05 -- more manpower from conquered land
+NDefines.NAI.BASE_RELUCTANCE = 2000 -- no military access - creates chaos and exploitable
